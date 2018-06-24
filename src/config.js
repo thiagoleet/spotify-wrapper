c2b978184a70c4c04d351eeafce4965b8d49a52c
Thiago Leite <thiagofmleite@gmail.com>
2018-06-24 13:41:49 -0300

    Adicionado suporte ao token

--- src/config.js
+++ src/config.js
@@ -1,2 +1,9 @@
-const API_URL = 'https://api.spotify.com/v1';
-export default API_URL;
+const TOKEN_API = '';
+
+export const API_URL = 'https://api.spotify.com/v1';
+
+export const HEADERS = {
+    headers: {
+        Authorization: `'Bearer ${TOKEN_API}'`,
+    },
+};
